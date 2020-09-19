ac5d9bd4077c6aacc5a91417da276f69ec5f90bd
Brisppy <harrison.crisp@crispgrp.com>
2020-09-19 12:40:13 +1000

    Improved recording what VODs are downloaded

--- twitch-vod-archiver.sh
+++ twitch-vod-archiver.sh
@@ -11,15 +11,14 @@ VOD_DIRECTORY= # Path to VOD Directory, do NOT end with a slash (/)
 SEND_PUSHBULLET= # 0/1, send Pushbullet notificaiton
 PUSHBULLET_KEY= # Pushbullet API key
 
-## DO NOT MODIFY
-DOWNLOADED_VODS=$(ls $VOD_DIRECTORY | sed 's/ /\n/g')
-
 # Call Twitch API and return the user_id of channel.
 USER_ID=$(curl -s -H "Authorization: Bearer $OAUTH_TOKEN" -H "Client-Id: $CLIENT_ID" -X GET https://api.twitch.tv/helix/users?login=$CHANNEL | jq '.data[].id' | sed 's/"//g')
 echo User $CHANNEL ID is $USER_ID
 
 # Return a list of available VODs from $CHANNEL
 AVAILABLE_VODS=$(curl -s -H "Authorization: Bearer $OAUTH_TOKEN" -H "Client-Id: $CLIENT_ID" -X GET https://api.twitch.tv/helix/videos?user_id=$USER_ID | jq '.data[].id' | sed 's/"//g' | sed 's/ /\n/g')
+# Return a list of downloaded VODs
+DOWNLOADED_VODS=$(cat $VOD_DIRECTORY/downloaded_vods)
 echo Available VODS:
 echo "$AVAILABLE_VODS"
 echo Downloaded VODS:
@@ -44,13 +43,13 @@ for VOD in $NEW_VODS; do
 	VOD_DURATION=$(echo $VOD_JSON | jq '.data[].duration' | sed 's/"//g')
 	echo VOD name is $VOD_NAME, created $VOD_DATE, and is $VOD_DURATION long.
 	# Create a directory for the VOD corresponding with its' id
-	mkdir $VOD_DIRECTORY/$VOD
+	mkdir "$VOD_DIRECTORY/$VOD - $VOD_NAME"
 	# Create a file containing the JSON and date.
-	echo "$VOD_JSON" > $VOD_DIRECTORY/$VOD/$VOD_DATE
+	echo "$VOD_JSON" > "$VOD_DIRECTORY/$VOD - $VOD_NAME/vod.json"
 	# Download the chat logs for the VOD
-	tcd --video $VOD --format irc  --client-id $APP_CLIENT_ID --client-secret $APP_CLIENT_SECRET --output $VOD_DIRECTORY/$VOD/chat_log.txt
+	tcd --video $VOD --format irc --client-id $APP_CLIENT_ID --client-secret $APP_CLIENT_SECRET --output "$VOD_DIRECTORY/$VOD - $VOD_NAME/"
 	# Download the VOD to the desired directory
-	streamlink --hls-segment-threads 4 https://twitch.tv/videos/$VOD best -o "$VOD_DIRECTORY/$VOD/$VOD_NAME.mp4" --twitch-oauth-token=$OAUTH_TOKEN
+	streamlink --hls-segment-threads 4 https://twitch.tv/videos/$VOD best -o "$VOD_DIRECTORY/$VOD - $VOD_NAME/$VOD_NAME.mp4" --twitch-oauth-token=$OAUTH_TOKEN
 	# Count the number of columns within the VOD_DURATION variable
 	VOD_DURATION_SPLIT=$(echo $VOD_DURATION | sed 's/h/:/g' | sed 's/m/:/g' | sed 's/s//g')
 	VOD_DURATION_COLUMNS=$(echo $VOD_DURATION_SPLIT | tr ':' '\n' | wc -l)
@@ -64,15 +63,17 @@ for VOD in $NEW_VODS; do
 	fi
 	echo Duration in seconds of VOD is $VOD_DURATION_SECONDS
 	# Get the length of the downloaded file
-	DOWNLOADED_DURATION=$(ffprobe -i "$VOD_DIRECTORY/$VOD/$VOD_NAME.mp4" -show_format -v quiet | sed -n 's/duration=//p' | xargs printf %.0f)
+	DOWNLOADED_DURATION=$(ffprobe -i "$VOD_DIRECTORY/$VOD - $VOD_NAME/$VOD_NAME.mp4" -show_format -v quiet | sed -n 's/duration=//p' | xargs printf %.0f)
 	echo Expected duration is "$VOD_DURATION_SECONDS"s, downloaded duration is "$DOWNLOADED_DURATION"s
 	# Compare the length of the VOD to the downloaded file
 	if [ "$DOWNLOADED_DURATION" -ge "$((VOD_DURATION_SECONDS - 10))" ] && [ "$DOWNLOADED_DURATION" -le "$((VOD_DURATION_SECONDS + 10))" ]; then
 		echo Files are within 10 seconds.
+		# Add VOD to file to keep track of what has been downloaded
+		echo $VOD >> $VOD_DIRECTORY/downloaded_vods
 	else
 		echo Files have different durations, removing the VOD folder, sending a notification and exiting...
 		[ $SEND_PUSHBULLET -eq 1 ] && curl -u $PUSHBULLET_KEY: -d type="note" -d body="Error archiving Twitch VOD $VOD from $VOD_DATE" -d title="Twitch VOD Archiver Error" 'https://api.pushbullet.com/v2/pushes'
-		rm -drf $VOD_DIRECTORY/$VOD
+		rm -drf "$VOD_DIRECTORY/$VOD - $VOD_NAME"
 		exit 1
 	fi
 done
