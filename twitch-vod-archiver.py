08b94b5c9c7234674afe1edcd4538118210b4dcd
Brisppy <harrison.crisp@crispgrp.com>
2021-09-01 17:16:43 +1000

    Initial support for live archiving

--- twitch-vod-archiver.py
+++ twitch-vod-archiver.py
@@ -14,6 +14,7 @@ from variables import *
 import subprocess
 import requests
 import shutil
+import time
 import math
 import glob
 import json
@@ -39,7 +40,7 @@ CHANNEL = sys.argv[1] # Channel name
 # This function is used for retrieving information from the Twitch API, returning an array containing the retrieved
 # information.
 # Takes 'api_path' as a variable, defining the API endpoint to call.
-def CallTwitch(api_path, pagination=0):
+def CallTwitch(api_path, pagination=0, live_mode=0):
     url = 'https://api.twitch.tv/helix/'
     headers = {'Authorization': 'Bearer ' + OAUTH_TOKEN, 'Client-Id': CLIENT_ID}
     try:
@@ -65,8 +66,11 @@ def CallTwitch(api_path, pagination=0):
         else:
             r = requests.get(url + api_path, headers=headers)
             if r.status_code != 200:
-                print('ERROR: Status code ' + r.status_code + ' received from Twitch.')
+                print('ERROR: Status code ' + str(r.status_code) + ' received from Twitch.')
                 print('ERROR:', r.text)
+                # If operating in live mode, we must move onto combining the VOD which was most likely deleted.
+                if live_mode and r.status_code == 404:
+                    return 404
                 sys.exit(1)
             return json.loads(r.text)
     except requests.exceptions.RequestException as e:
@@ -76,14 +80,15 @@ def CallTwitch(api_path, pagination=0):
 
 # This function is used to retrieve the chat logs for a particular VOD.
 # Takes VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET and VOD_SUBDIR
-def RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR):
+def RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR, LIVE_MODE):
     ATTEMPT = 0
     while True:
         ATTEMPT += 1
         if ATTEMPT > 5:
             print('ERROR: Failed to download chat log after', ATTEMPT, 'attempts.')
             if SEND_PUSHBULLET:
-                SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Failed to Download Chat Log.')
+                SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Failed to Download Chat Log. This may be corrected on next '
+                                                         'run')
             sys.exit(1)
         # Call twitch-chat-downloader
         p = subprocess.run('tcd --video ' + VOD_INFO['id'] + ' --format irc --client-id ' + APP_CLIENT_ID + 
@@ -91,8 +96,13 @@ def RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR):
                            shell=True)
         # Check if tcd returned an error
         if p.returncode:
-            print('ERROR: twitch-chat-downloader exited with error.')
-            continue
+            if not LIVE_MODE:
+                print('ERROR: twitch-chat-downloader exited with error.')
+                continue
+            else:
+                print('INFO: Chat failed to download, but we are running in LIVE mode. The VOD (and chat) may have '
+                      'been deleted.')
+                return
         else:
             print('INFO: Chat downloaded successfully.')
             return
@@ -100,21 +110,54 @@ def RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR):
 
 # This function is used to retrieve the actual video files for the VOD and combine them.
 # Takes VOD_INFO, VOD_SUBDIR and VOD_NAME.
-def RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_NAME):
+def RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_NAME, LIVE_MODE):
     # This must be set to choose where the VOD is downloaded to before merging
     d = dict(os.environ)
     d['TMPDIR'] = str(VOD_SUBDIR)
-    # Actual command for downloading the VOD
-    p = subprocess.run('twitch-dl download --max-workers 20 --no-join -q source ' + VOD_INFO['id'], shell=True, env=d)
-    # Catch any exit code other than 0
-    if p.returncode:
-        print('ERROR: VOD download exited with error.')
-        print('ERROR:', p.stdout)
-        if SEND_PUSHBULLET:
-            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'VOD download exited with error.')
-        sys.exit(1)
-    else:
-        print('INFO: VOD download successful.')
+    # We grab the current duration of the VOD first to check against later.
+    CUR_VOD_DURATION = VOD_INFO['duration']
+    while True:
+        # If VOD is live and under 5m long, we wait to ensure the VOD is ready for download
+        if LIVE_MODE and ConvertToSeconds(CallTwitch('videos?id=' + str(VOD_INFO['id']))['data'][0]['duration']) < 300:
+            print('INFO: VOD is not currently long enough to download, pausing for 5 minutes.')
+            time.sleep(300)
+            continue
+        # Actual command for downloading the VOD
+        p = subprocess.run('twitch-dl download --max-workers 20 --no-join -q source ' + VOD_INFO['id'], shell=True,
+                                                                                                        env=d)
+        # Catch any exit code other than 0
+        if p.returncode:
+            print('ERROR: VOD download exited with error.')
+            print('ERROR:', p.stdout)
+            if SEND_PUSHBULLET:
+                SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'VOD download exited with error. Check log and remove .lock.' +
+                               str(VOD_INFO['id']) + ' file.')
+            sys.exit(1)
+        if LIVE_MODE:
+            # Fetch the current VOD duration again. Sleep first to allow the Twitch servers to adjust duration.
+            print('INFO: Waiting 3 minutes for VOD length to update on Twitch servers.')
+            time.sleep(180)
+            NEW_VOD_INFO = CallTwitch('videos?id=' + str(VOD_INFO['id']), False, True)
+            # If a 404 is returned, the VOD no longer exists, we continue as if we have the entire VOD saved
+            if NEW_VOD_INFO == 404:
+                print('INFO: 404 Returned from Twitch, VOD was most likely deleted. Creating .ignorelength file as '
+                      'duration will most likely be incorrect.')
+                with open(Path(VOD_SUBDIR, '.ignorelength'), 'w') as ignorelength:
+                    pass
+                # Also add the most recently grabbed duration to the RAW_VOD_DATA array
+                RAW_VOD_INFO['duration'] = CUR_VOD_DURATION
+                break
+            else: 
+                NEW_VOD_DURATION = NEW_VOD_INFO['data'][0]['duration']
+            # Compare the original duration to the newly fetched duration
+            if CUR_VOD_DURATION != NEW_VOD_DURATION:
+                print('INFO: VOD Duration has changed (Probably live) - attempting to download VOD again.')
+                print('DEBUG: Previous duration:', CUR_VOD_DURATION, 'New duration:', NEW_VOD_DURATION)
+                CUR_VOD_DURATION = NEW_VOD_DURATION
+                continue
+        else:
+            print('INFO: VOD download successful.')
+            break
     # Create a list containing all VOD .ts files - requires glob as the pathnames are difficult to decipher, and so
     # a wildcard is the preferred choice.
     VOD_PARTS = [Path(p) for p in sorted(glob.glob(str(VOD_SUBDIR) + '/twitch-dl/*/chunked/*.ts'))]
@@ -133,17 +176,19 @@ def RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_NAME):
         print('ERROR: Combining of downloaded .ts files failed.')
         print('ERROR:', e)
         if SEND_PUSHBULLET:
-            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Error Combining .ts Files.')
+            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Error Combining .ts Files. Check log and remove .lock.' +
+                           str(VOD_INFO['id']) + ' file.')
         sys.exit(1)
     # Remux the combined .ts file into a .mp4
     print('INFO: Remuxing VOD file into to a .mp4 file.')
-    p = subprocess.run('ffmpeg -y -i ' + '"' + str(Path(VOD_SUBDIR, 'merged.ts')) + '"' + ' -c:a copy -c:v copy ' + 
-                       '"' + str(Path(VOD_SUBDIR, VOD_NAME + '.mp4')) + '"', shell=True)
+    p = subprocess.run('ffmpeg -v quiet -y -i ' + '"' + str(Path(VOD_SUBDIR, 'merged.ts')) + '"' +
+                       ' -c:a copy -c:v copy ' + '"' + str(Path(VOD_SUBDIR, VOD_NAME + '.mp4')) + '"', shell=True)
     # Catch any non-zero exit code
     if p.returncode:
         print('ERROR: VOD remuxing exited with error.')
         if SEND_PUSHBULLET:
-            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Error Remuxing Merged File.')
+            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Error Remuxing Merged File. Check log and remove .lock.' +
+                           str(VOD_INFO['id']) + ' file.')
         sys.exit(1)
     else:
         print('INFO: VOD combined successfully.')
@@ -154,13 +199,7 @@ def RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_NAME):
 # Takes VOD_INFO, VOD_NAME and VOD_SUBDIR.
 def VerifyVODLength(VOD_INFO, VOD_NAME, VOD_SUBDIR):
     # First we convert the provided time from the format '00h00m00s' to just seconds
-    VOD_LENGTH = VOD_INFO['duration'].replace('h', ':').replace('m', ':').replace('s', '').split(':')
-    if len(VOD_LENGTH) == 1:
-        VOD_DURATION_SECONDS = int(VOD_LENGTH[0])
-    elif len(VOD_LENGTH) == 2:
-        VOD_DURATION_SECONDS = (int(VOD_LENGTH[0]) * 60) + int(VOD_LENGTH[1])
-    elif len(VOD_LENGTH) == 3:
-        VOD_DURATION_SECONDS = (int(VOD_LENGTH[0]) * 3600) + (int(VOD_LENGTH[1]) * 60) + int(VOD_LENGTH[2])
+    VOD_DURATION_SECONDS = ConvertToSeconds(VOD_INFO['duration'])
     # Retrieve the duration of the downloaded VOD
     p = subprocess.run('ffprobe -i ' + '"' + str(Path(VOD_SUBDIR, VOD_NAME + '.mp4')) + '"' +
                        ' -v quiet -show_entries format=duration -of default=noprint_wrappers=1:nokey=1', 
@@ -199,7 +238,8 @@ def VerifyVODLength(VOD_INFO, VOD_NAME, VOD_SUBDIR):
     else:
         print('ERROR: Downloaded VOD duration less than expected duration.')
         if SEND_PUSHBULLET:
-            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Downloaded VOD duration shorter than expected.')
+            SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Downloaded VOD duration shorter than expected. Check log '
+                          'and remove .lock.' + str(VOD_INFO['id']) + ' file.')
         sys.exit(1)
 
 
@@ -213,6 +253,17 @@ def SendPushbullet(PUSHBULLET_KEY, VOD_INFO, ERROR):
     data_send = {"type": "note", "title": 'Error Archiving Twitch VOD ' + VOD_INFO['id'] + ' by ' +
                  VOD_INFO['user_name'], "body": ERROR}
     _r = requests.post(url, headers=headers, data=json.dumps(data_send))
+
+
+# Convert duration from the format XXhXXmXXs to just seconds.
+def ConvertToSeconds(duration):
+    duration = duration.replace('h', ':').replace('m', ':').replace('s', '').split(':')
+    if len(duration) == 1:
+        return int(duration[0])
+    elif len(duration) == 2:
+        return (int(duration[0]) * 60) + int(duration[1])
+    elif len(duration) == 3:
+        return (int(duration[0]) * 3600) + (int(duration[1]) * 60) + int(duration[2])
 
 
 # This is the main function used for retrieving VOD information.
@@ -236,19 +287,6 @@ def main():
         print('ERROR: No USER_ID received from Twitch, check your connection to Twitch and spelling of channel name.')
         sys.exit(1)
     print('INFO: User ' + CHANNEL +' ID is ' + USER_ID + '.')
-    # Check if the channel is currently live - if so we must ignore the most recent VOD as it is still being added to.
-    CHANNEL_STATUS = CallTwitch('streams?user_id=' + USER_ID)
-    if CHANNEL_STATUS['data']:
-        if CHANNEL_STATUS['data'][0]['type'] == 'live':
-            print('INFO: Channel is currently live. Assuming the highest numbered VOD is still being generated and \
-                  ignoring.')
-            CHANNEL_LIVE = True
-        else:
-            print('INFO: Channel is live, but may not be streaming (And creating a VOD).')
-            CHANNEL_LIVE = False
-    else:
-        print('INFO: Channel is currently offline.')
-        CHANNEL_LIVE = False
     # Return a list of available VODs from $CHANNEL
     AVAIL_VODS = CallTwitch('videos?user_id=' + USER_ID + '&first=100&type=archive', 1)
     # Create a list of available VODs
@@ -256,9 +294,18 @@ def main():
     for vod in AVAIL_VODS['data']:
         AVAILABLE_VODS.append(int(vod['id']))
     print('INFO: Available VODs:', AVAILABLE_VODS)
-    # Remove highest numbered VOD is channel is currently live
-    if CHANNEL_LIVE:
-        AVAILABLE_VODS.remove(max(AVAILABLE_VODS))
+    # Check if the channel is currently live.
+    CHANNEL_STATUS = CallTwitch('streams?user_id=' + USER_ID)
+    if CHANNEL_STATUS['data']:
+        if CHANNEL_STATUS['data'][0]['type'] == 'live':
+            print('INFO: Channel is currently live.')
+            CHANNEL_LIVE = True
+        else:
+            print('INFO: Channel is live, but may not be streaming (Could be a rerun).')
+            CHANNEL_LIVE = False
+    else:
+        print('INFO: Channel is currently offline.')
+        CHANNEL_LIVE = False
     # Retrieve currently downloaded VODs from VOD database
     select_vods = 'SELECT * from vods'
     DOWN_VODS = ExecuteReadQuery(database_file, select_vods)
@@ -279,7 +326,24 @@ def main():
     for vod_id in VOD_QUEUE:
         print('INFO: Retrieving VOD:', vod_id)
         VOD_INFO = CallTwitch('videos?id=' + str(vod_id))['data'][0]
+        # We need to modify the duration if the VOD is live from with in the RetrieveVODVideo function. There is
+        # probably a better method of doing this than declaring a global variable.
+        global RAW_VOD_INFO
         RAW_VOD_INFO = copy.deepcopy(VOD_INFO)
+        # Check if lock file exists and move to the next VOD if it does
+        if os.path.isfile(Path(VOD_DIRECTORY, VOD_INFO['user_name'], '.lock.' + str(vod_id))):
+            print('INFO: Lock file present for vod ' + str(vod_id) + '. VOD either failed previously with an error, or '
+                  'is still being processed by another instance of TVA.')
+            continue
+        else:
+            # Create lock file
+            with open(Path(VOD_DIRECTORY, VOD_INFO['user_name'], '.lock.' + str(vod_id)), 'w') as lockfile:
+                pass
+        if CHANNEL_LIVE and vod_id == VOD_QUEUE[0]:
+            print('INFO: Selected VOD is still being updated, running in LIVE mode.')
+            LIVE_MODE = True
+        else:
+            LIVE_MODE = False
         # Sanitize the dates
         VOD_INFO['created_at'] = VOD_INFO['created_at'].replace(':', '_')
         VOD_INFO['published_at'] = VOD_INFO['published_at'].replace(':', '_')
@@ -295,10 +359,10 @@ def main():
         if not os.path.isdir(Path(VOD_SUBDIR)):
             print('INFO: Creating individual VOD directory.')
             os.mkdir(Path(VOD_SUBDIR))
-        # First we download the chat logs
-        RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR)
-        # Then we grab the video
-        RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_INFO['title'])
+        # First we grab video
+        RetrieveVODVideo(VOD_INFO, VOD_SUBDIR, VOD_INFO['title'], LIVE_MODE)
+        # Then we download the chat logs after in case the download is in LIVE mode
+        RetrieveVODChat(VOD_INFO, APP_CLIENT_ID, APP_CLIENT_SECRET, VOD_SUBDIR, LIVE_MODE)
         # Now we make sure the VOD length matches what is expected
         VerifyVODLength(VOD_INFO, VOD_INFO['title'], VOD_SUBDIR)
         # If we've made it to this point, all files have been downloaded and the VOD can be added to the database.
@@ -315,9 +379,13 @@ def main():
         if ExecuteQuery(database_file, create_vod, list(RAW_VOD_INFO.values())):
             print('ERROR: Failed to add VOD information to database.')
             if SEND_PUSHBULLET:
-                SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Failed to add VOD information to database.')
+                SendPushbullet(PUSHBULLET_KEY, VOD_INFO, 'Failed to add VOD information to database. Lock file removed '
+                                                         'as the error may correct itself next run.')
             sys.exit(1)
         else:
             print('INFO: VOD ' + VOD_INFO['id'] + ' successfully downloaded.')
+            # Remove lock file
+            if os.path.isfile(Path(VOD_DIRECTORY, VOD_INFO['user_name'], '.lock.' + str(vod_id))):
+                os.remove(Path(VOD_DIRECTORY, VOD_INFO['user_name'], '.lock.' + str(vod_id)))
 
 main()
