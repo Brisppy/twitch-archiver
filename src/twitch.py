Brisppy/twitch-archiver

commit 89fe4d8442add294e1141d5b1162c24608c73ae3

Small formatting adjustments

src/twitch.py
@@ -15,12 +15,11 @@ class Twitch:
 
         :param config: configuration dict generated by Config
         """
-
         self.log = logging.getLogger('twitch-archive')
 
-        self.Api = Api(config['pushbullet_key'])
-
         self.Config = config
+
+        self.Api = self.Config['pushbullet_key']
 
     def get_api(self, api_path):
         """Retrieves information from the Twitch API.
@@ -63,7 +62,7 @@ class Twitch:
         :return: playback access token
         """
         # only accepts the default client ID for non-authenticated clients
-        _h = {'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko'} 
+        _h = {'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko'}
         _q = """
         {{
             videoPlaybackAccessToken(

src/configuration.py
@@ -96,14 +96,14 @@ class Configuration:
 
         return Configuration.__conf[name]
 
-    # reference: 
+    # reference:
     #   https://stackoverflow.com/questions/35247900/python-creating-an-ini-or-config-file-in-the-users-home-directory
     def save(self, conf_file, name=None):
         """
         Saves the running configuration to the configuration ini.
         """
         self.log.debug('Saving config variable(s) to ini file.')
-        
+
         # import saved config
         config = configparser.ConfigParser()
         config.read(conf_file)

commit e31593b644c4da2be6e00d1ef7cbaf7e24e7c441

Fix incorrect module call introduced by 89fe4d8442add294e1141d5b1162c24608c73ae3

src/twitch.py
@@ -19,7 +19,7 @@ class Twitch:
 
         self.Config = config
 
-        self.Api = self.Config['pushbullet_key']
+        self.Api = Api(self.Config['pushbullet_key'])
 
     def get_api(self, api_path):
         """Retrieves information from the Twitch API.