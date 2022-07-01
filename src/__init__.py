6028e4165b0b71269227b8081b70ad8eebce71c7
Brisppy <49708988+Brisppy@users.noreply.github.com>
2022-07-01 14:49:31 +1000

    Sanitized credentials from arguments in debug logs

--- src/__init__.py
+++ src/__init__.py
@@ -82,7 +82,13 @@ def main():
     # setup logging
     log = Logger.setup_logger(args.get('quiet') + args.get('debug'), args.get('log_file'))
     log.debug('Debug logging enabled.')
-    log.debug(f'Arguments: {args.get()}')
+
+    args_sanitized = args.get()
+    for key in ['client_id', 'client_secret', 'pushbullet_key']:
+        if args_sanitized[key]:
+            args_sanitized.update({key: 24 * '*' + args_sanitized[key][24:]})
+
+    log.debug(f'Arguments: {args_sanitized}')
 
     # compare with current git version
     latest_version, release_notes = Utils.get_latest_version()
