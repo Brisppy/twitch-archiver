db2591dddd83a420b8a9dbff37f412656ce42baf
Brisppy <49708988+Brisppy@users.noreply.github.com>
2023-12-20 15:17:23 +1100

    Fix freshly live streams raising an exception before other archival methods could be attempted

--- twitcharchiver/downloaders/stream.py
+++ twitcharchiver/downloaders/stream.py
@@ -350,7 +350,10 @@ class Stream(Downloader):
         # meantime in case there is no vod and thus no way to retrieve them after the fact
         if not self.vod.v_id:
             if self.vod.duration < TEMP_BUFFER_LEN:
-                self._buffer_stream(self.vod.duration)
+                try:
+                    self._buffer_stream(self.vod.duration)
+                except UnsupportedStreamPartDuration:
+                    self._log.debug('Failed to buffer stream as parts have an unsupported duration.')
 
             # fetch most recent channel broadcast ID
             broadcast_vod_id = self.channel.broadcast_v_id
