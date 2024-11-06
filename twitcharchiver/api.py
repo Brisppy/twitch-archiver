5d77ccd7472261140afa3976897692d8685c342e
Brisppy <49708988+Brisppy@users.noreply.github.com>
2024-11-06 11:44:20 +1100

    Refactor API class into singleton

--- twitcharchiver/api.py
+++ twitcharchiver/api.py
@@ -20,7 +20,16 @@ class Api:
     Handles communication with the Twitch API.
     """
 
-    def __init__(self):
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super(Api, cls).__new__(cls)
+            cls.__instance.__initialize()
+
+        return cls.__instance
+
+    def __initialize(self):
         """
         Class constructor.
         """
